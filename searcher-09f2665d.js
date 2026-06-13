4681f3aa0dfb30c22d53301820a12926879652f5
Vanuan <Vanuan@users.noreply.github.com>
2026-06-13 12:40:26 +0000

    deploy: 288f9cbec59e479f70dec9ae96f4fff0c4d63537

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-566caf01.js',
+                path_to_root + 'searchindex-73db92df.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
